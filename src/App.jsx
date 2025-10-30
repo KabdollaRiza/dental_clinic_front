24fb1647e28070a8418a3b8b37c5764c73f4fd3d
Riza <230023@astanait.edu.kz>
2025-10-30 17:28:20 +0800

    Add main App component and entry point

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
